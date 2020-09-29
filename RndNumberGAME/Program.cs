ddcbfc83eb8ec0c2ff7039ebdb26edea49830ee8
Administrator@plumbworldnetwork.co.uk <Administrator@plumbworldnetwork.co.uk>
2020-09-29 19:26:00 +0100

    Add project files.

---
+++ RndNumberGAME/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RndNumberGAME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guesslimit = 10;
+
+
+            Console.WriteLine("Please pick a random number");
+            string guess = Console.ReadLine();
+            
+            int guessint = Int32.Parse(guess);
+
+            Random random = new Random();
+
+            int random1 = random.Next(1, 10);
+
+
+            if (guessint == random1)
+            {
+                Console.WriteLine("You picked the correct number");
+            }
+            else if (guessint > random1)
+            {
+                Console.WriteLine("You Number was to High.");
+            }
+            else if (guessint < random1)
+            {
+                Console.WriteLine("You Number was to Low.");
+            }
+
+
+
+            Console.WriteLine(random1);
+
+
+
+        }
+    }
+}
